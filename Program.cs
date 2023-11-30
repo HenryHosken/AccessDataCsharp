67a6be0707b36a2153e3f5a7e6d335fbfa4b3aee
henry hosken <henryhosken13@gmail.com>
2023-11-30 16:57:30 -0300

    ..

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using BaltaDataAccess.Models;
 using Dapper;
 using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace BaltaDataAccess
 {
@@ -12,7 +13,12 @@ namespace BaltaDataAccess
 
             using (var connection = new SqlConnection(connectionString))
             {
-
+                //UpdateCategory(connection);
+                //CreateManyCategory(connection);
+                //ListCategories(connection);
+                //DeleteCategory(connection);
+                //GetCategory(connection);
+                //ExecuteReadProcedure(connection);
             }
         }
 
@@ -26,10 +32,14 @@ namespace BaltaDataAccess
 
         }
 
-        static void CreateCategory(SqlConnection connection)
+        static void GetCategory(SqlConnection connection)
+        {
+
+        }
+
+        static void CreateManyCategory(SqlConnection connection)
         {
             var category = new Category();
-            category.Id = Guid.NewGuid();
             category.Title = "Amazon AWS";
             category.Url = "amazon";
             category.Description = "Categoria de serviços do AWS";
@@ -37,12 +47,22 @@ namespace BaltaDataAccess
             category.Summary = "AWS Cloud";
             category.Featured = false;
 
+            //
+
+            var category2 = new Category();
+            category2.Title = "Categoria nova";
+            category2.Url = "Categoria-nova";
+            category2.Description = "Categoria nova";
+            category2.Order = 9;
+            category2.Summary = "categoriaa";
+            category2.Featured = true;
+
             // SQL Injection
 
             var insertSql = @"INSERT INTO 
                     [Category] 
                 VALUES(
-                    @Id,    
+                    NEWID(),    
                     @Title,
                     @Url,
                     @Summary, 
@@ -50,17 +70,64 @@ namespace BaltaDataAccess
                     @Description,
                     @Featured)";
 
-            var rows = connection.Execute(insertSql, new
-            {
-                category.Id,
-                category.Title,
-                category.Url,
-                category.Summary,
-                category.Order,
-                category.Description,
-                category.Featured
+            var rows = connection.Execute(insertSql, new[]{
+                new
+                {
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                },
+                new
+                {
+                    category2.Title,
+                    category2.Url,
+                    category2.Summary,
+                    category2.Order,
+                    category2.Description,
+                    category2.Featured
+                }
             });
+
             Console.WriteLine($"{rows} linhas inseridas");
+        }
+
+        static void UpdateCategory(SqlConnection connection)
+        {
+            var updateQuerry = "UPDATE [Category] SET [Title] =@title WHERE [Id]=@id";
+            var rows = connection.Execute(updateQuerry, new
+            {
+                id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+                title = "Fronted 2021"
+            });
+
+            Console.WriteLine($"{rows} registros atualizados");
+        }
+
+        static void DeleteCategory(SqlConnection connection)
+        {
+
+        }
+
+        static void ExecuteReadProcedure(SqlConnection connection)
+        {
+            var procedure = "[spGetCoursesByCategory]";
+            var pars = new { CategoryId = "09ce0b7b-cfca-497b-92c0-3290ad9d5142" };
+            var Courses = connection.Query(
+                procedure,
+                pars,
+                commandType: CommandType.StoredProcedure);
+            foreach (var c in Courses)
+            {
+                Console.WriteLine($"Id= {c.Id} titutlo: {c.Title}");
+
+            }
+        }
+
+        static void ExecuteScalar(SqlConnection connection)
+        {
         }
     }
 }
